0b9f3d766bd03c0319897dbdcdf3625074e59fa6
kimyu0218 <serena35@ewhain.net>
2024-07-21 03:01:25 +0900

    refactor: record로 변경

--- src/main/java/com/nexters/goalpanzi/config/jwt/Jwt.java
+++ src/main/java/com/nexters/goalpanzi/config/jwt/Jwt.java
@@ -1,18 +1,7 @@
 package com.nexters.goalpanzi.config.jwt;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public class Jwt {
-    private final String accessToken;
-    private final String refreshToken;
-    private final long refreshExpiresIn;
-
-    @Builder
-    public Jwt(String accessToken, String refreshToken, long refreshExpiresIn) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        this.refreshExpiresIn = refreshExpiresIn;
-    }
+@Builder
+public record Jwt(String accessToken, String refreshToken, Long refreshExpiresIn) {
 }
