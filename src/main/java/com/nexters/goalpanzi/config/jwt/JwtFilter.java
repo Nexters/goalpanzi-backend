9b07944f7e08df0eeea1b5e0d93578d8db3a3a57
kimyu0218 <serena35@ewhain.net>
2024-07-20 11:29:28 +0900

    feat: JWT 필터 정의
    request 헤더에서 JWT 토큰을 추출하고 유효성 검사
    - 로그인은 인증 받지 않은 사용자가 이용할 수 있으므로 whitelist에
      추가하여 필터링 예외 처리
    - 토큰 유효성 검사 후 subject 정보를 request의 uuid 속성에 저장하여
      controller 단에서 이용할 수 있도록 구현
    - 토큰 유효성 검사가 실패한 경우 401 반환

---
+++ src/main/java/com/nexters/goalpanzi/config/jwt/JwtFilter.java
@@ -0,0 +1,84 @@
+package com.nexters.goalpanzi.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtFilter extends GenericFilterBean {
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String AUTHORIZATION_SCHEMA = "Bearer ";
+  private static final String TOKEN_REISSUE_URI = "/api/auth/token:reissue";
+  private static final String CONTENT_TYPE = "application/json";
+  private static final String ATTRIBUTE_NAME = "uuid";
+  private static final String UNAUTHORIZED_MESSAGE = "{\"error\": \"Unauthorized\"}";
+
+  private static final List<String> whitelist = List.of("/api/auth/login");
+
+  private final JwtUtil jwtUtil;
+
+  public JwtFilter(JwtUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
+  }
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+      throws IOException, ServletException {
+    HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+    HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+    String requestURI = httpServletRequest.getRequestURI();
+    if(isWhitelisted(requestURI)) {
+      filterChain.doFilter(servletRequest, servletResponse);
+      return;
+    }
+
+    String token = resolveToken(httpServletRequest);
+    if(isTokenReissueRequest(requestURI, token)) {
+      httpServletRequest.setAttribute(ATTRIBUTE_NAME, jwtUtil.getSubject(token));
+      filterChain.doFilter(servletRequest, servletResponse);
+      return;
+    }
+
+    if (isAuthenticated(token)) {
+      httpServletRequest.setAttribute(ATTRIBUTE_NAME, jwtUtil.getSubject(token));
+      filterChain.doFilter(servletRequest, servletResponse);
+      return;
+    }
+    httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    httpServletResponse.setContentType(CONTENT_TYPE);
+    httpServletResponse.getWriter().write(UNAUTHORIZED_MESSAGE);
+  }
+
+  private String resolveToken(HttpServletRequest request) {
+    String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+    if (isBearerToken(authorizationHeader)) {
+      return authorizationHeader.substring(AUTHORIZATION_SCHEMA.length());
+    }
+    return null;
+  }
+
+  private Boolean isBearerToken(String header) {
+    return StringUtils.hasText(header) && header.startsWith(AUTHORIZATION_SCHEMA);
+  }
+
+  private Boolean isTokenReissueRequest(String requestURI, String token) {
+    return requestURI.equals(TOKEN_REISSUE_URI)
+        && StringUtils.hasText(token)
+        && jwtUtil.isExpired(token);
+  }
+
+  private Boolean isAuthenticated(String token) {
+    return StringUtils.hasText(token) && jwtUtil.validateToken(token);
+  }
+
+  private Boolean isWhitelisted(String requestURI) {
+    return whitelist.stream().anyMatch(requestURI::startsWith);
+  }
+}
---
+++ src/test/java/com/nexters/goalpanzi/config/jwt/JwtFilterTest.java
@@ -0,0 +1,46 @@
+package com.nexters.goalpanzi.config.jwt;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@WebMvcTest
+public class JwtFilterTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Configuration
+  static class TestConfig implements WebMvcConfigurer {
+
+    @Bean
+    public JwtUtil jwtUtil() {
+      String secret = "test-secret";
+      long accessExpiresIn = 86400000;
+      long refreshExpiresIn = 1209600000;
+      return new JwtUtil(secret, accessExpiresIn, refreshExpiresIn);
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+      FilterRegistrationBean<JwtFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+      filterRegistrationBean.setFilter(new JwtFilter(jwtUtil()));
+      filterRegistrationBean.addUrlPatterns("/api/*");
+      return filterRegistrationBean;
+    }
+  }
+
+  @Test
+  void unauthorizedRequest() throws Exception {
+    mockMvc.perform(get("/api/auth/logout"))
+        .andExpect(status().isUnauthorized());
+  }
+}
