7dde7f5df6d3df01be4e8569db9c8fd273c7bb0a
Songyi Kim <52441906+songyi00@users.noreply.github.com>
2024-07-21 01:49:25 +0900

    refactor: 불필요한 코드 정리 (#8)

--- src/main/java/com/nexters/goalpanzi/application/auth/AuthService.java
+++ src/main/java/com/nexters/goalpanzi/application/auth/AuthService.java
@@ -1,7 +1,7 @@
 package com.nexters.goalpanzi.application.auth;
 
 import com.nexters.goalpanzi.application.auth.dto.AppleLoginRequest;
-import com.nexters.goalpanzi.application.auth.dto.JwtTokenResponse;
+import com.nexters.goalpanzi.application.auth.dto.JwtResponse;
 import com.nexters.goalpanzi.domain.member.Member;
 import com.nexters.goalpanzi.domain.member.MemberRepository;
 import com.nexters.goalpanzi.domain.member.SocialType;
@@ -15,7 +15,7 @@ public class AuthService {
     private final SocialUserProviderFactory socialUserProviderFactory;
     private final MemberRepository memberRepository;
 
-    public JwtTokenResponse appleOAuthLogin(final AppleLoginRequest request) {
+    public JwtResponse appleOAuthLogin(final AppleLoginRequest request) {
         SocialUserProvider appleUserProvider = socialUserProviderFactory.getProvider(SocialType.APPLE);
         SocialUserInfo socialUserInfo = appleUserProvider.getSocialUserInfo(request.identityToken());
 
@@ -23,7 +23,7 @@ public class AuthService {
 
         // TODO: accessToken, refreshToken 발급
 
-        return new JwtTokenResponse("TODO", "TODO", member.isProfileSet());
+        return new JwtResponse("TODO", "TODO", member.isProfileSet());
     }
 
     public void googleOAuthLogin() {
--- src/main/java/com/nexters/goalpanzi/domain/member/SocialType.java
+++ src/main/java/com/nexters/goalpanzi/domain/member/SocialType.java
@@ -4,11 +4,5 @@ import lombok.Getter;
 
 @Getter
 public enum SocialType {
-    GOOGLE("TODO"), APPLE("appleUserInfoProvider");
-
-    private final String userInfoProvider;
-
-    SocialType(String userInfoProvider) {
-        this.userInfoProvider = userInfoProvider;
-    }
+    GOOGLE, APPLE
 }
