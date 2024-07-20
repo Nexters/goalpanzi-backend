45665599baed60a79f49b40f72912398858466c1
kimyu0218 <serena35@ewhain.net>
2024-07-20 11:28:34 +0900

    feat: JWT 클래스 정의
    access 토큰, refresh 토큰, refresh 토큰 만료 기한 필드 정의

---
+++ src/main/java/com/nexters/goalpanzi/config/jwt/Jwt.java
@@ -0,0 +1,18 @@
+package com.nexters.goalpanzi.config.jwt;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Jwt {
+  private final String accessToken;
+  private final String refreshToken;
+  private final long refreshExpiresIn;
+
+  @Builder
+  public Jwt(String accessToken, String refreshToken, long refreshExpiresIn) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.refreshExpiresIn = refreshExpiresIn;
+  }
+}
