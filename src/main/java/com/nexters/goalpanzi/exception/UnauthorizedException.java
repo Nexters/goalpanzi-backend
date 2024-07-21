437cd0d6a86b8003414233101b15235a31ad9b62
kimyu0218 <serena35@ewhain.net>
2024-07-21 23:11:14 +0900

    feat: UnauthorizedException 정의

---
+++ src/main/java/com/nexters/goalpanzi/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.nexters.goalpanzi.exception;
+
+public class UnauthorizedException extends BaseException {
+    public UnauthorizedException(final String message) {
+        super(message);
+    }
+}
