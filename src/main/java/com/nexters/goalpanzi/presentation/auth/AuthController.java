f286e6b2117e7b2e5d34214692c04ec40fff680f
kimyu0218 <serena35@ewhain.net>
2024-07-22 00:03:04 +0900

    feat: 컨트롤러 작성
    구글 로그인, 로그아웃, 토큰 재발급

--- src/main/java/com/nexters/goalpanzi/presentation/auth/AuthController.java
+++ src/main/java/com/nexters/goalpanzi/presentation/auth/AuthController.java
@@ -1,10 +1,11 @@
 package com.nexters.goalpanzi.presentation.auth;
 
 import com.nexters.goalpanzi.application.auth.AuthService;
-import com.nexters.goalpanzi.application.auth.dto.AppleLoginRequest;
-import com.nexters.goalpanzi.application.auth.dto.JwtResponse;
+import com.nexters.goalpanzi.application.auth.dto.*;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,11 +21,39 @@ public class AuthController implements AuthControllerDocs {
 
     @Override
     @PostMapping("/login/apple")
-    public ResponseEntity<JwtResponse> loginApple(
+    public ResponseEntity<LoginResponse> loginApple(
             @RequestBody @Valid final AppleLoginRequest appleLoginRequest
     ) {
-        JwtResponse response = authService.appleOAuthLogin(appleLoginRequest);
+        LoginResponse response = authService.appleOAuthLogin(appleLoginRequest);
 
         return ResponseEntity.ok(response);
+    }
+
+    @Override
+    @PostMapping("/login/google")
+    public ResponseEntity<LoginResponse> loginGoogle(
+            @RequestBody @Valid final GoogleLoginRequest googleLoginRequest
+    ) {
+        LoginResponse response = authService.googleOAuthLogin(googleLoginRequest);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<Void> logout(final HttpServletRequest request) {
+        authService.logout(request.getHeader("altKey"));
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    @PostMapping("/token:reissue")
+    public ResponseEntity<TokenResponse> reissueToken(
+            @RequestBody @Valid final TokenRequest tokenRequest,
+            final HttpServletRequest request
+    ) {
+        TokenResponse tokenResponse = authService.reissueToken(request.getHeader("altKey"), tokenRequest.refreshToken());
+
+        return ResponseEntity.ok(tokenResponse);
     }
 }
