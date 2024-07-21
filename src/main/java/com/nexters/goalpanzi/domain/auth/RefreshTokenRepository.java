b20085a6d0b9b41c5b4f27e3077b14a206f48b01
kimyu0218 <serena35@ewhain.net>
2024-07-21 19:31:55 +0900

    move: 상위 디렉토리 변경
    refreshToken -> auth

---
+++ src/main/java/com/nexters/goalpanzi/domain/auth/RefreshTokenRepository.java
@@ -0,0 +1,35 @@
+package com.nexters.goalpanzi.domain.auth;
+
+import com.nexters.goalpanzi.domain.common.RedisRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRepository implements RedisRepository {
+
+    private static final String REFRESH_TOKEN_POSTFIX = ":refresh_token";
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void save(String altKey, String refreshToken, long ttl) {
+        String key = makeKey(altKey);
+        redisTemplate.opsForValue().set(key, refreshToken, Duration.ofMillis(ttl));
+    }
+
+    public String find(String altKey) {
+        String key = makeKey(altKey);
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public Boolean delete(String altKey) {
+        return redisTemplate.delete(altKey);
+    }
+
+    private String makeKey(String altKey) {
+        return altKey + REFRESH_TOKEN_POSTFIX;
+    }
+}
