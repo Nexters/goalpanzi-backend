704484b0b5fd5b4b5cef1617084f51cbae4276c2
kimyu0218 <serena35@ewhain.net>
2024-07-22 23:43:36 +0900

    feat: refresh token 관련 에러 코드 및 생성자 추가

--- src/main/java/com/nexters/goalpanzi/exception/ErrorCode.java
+++ src/main/java/com/nexters/goalpanzi/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     INVALID_APPLE_TOKEN("애플 토큰 검증에 실패하였습니다."),
     EXPIRED_APPLE_TOKEN("애플 토큰이 만료되었습니다."),
 
-    INVALID_TOKEN("토큰 검증에 실패하였습니다.");
+    INVALID_TOKEN("토큰 검증에 실패하였습니다."),
+    INVALID_REFRESH_TOKEN("refresh 토큰이 갱신되어 더 이상 유효하지 않은 refresh 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN("만료된 refresh 토큰입니다.");
 
     private String message;
 }
--- src/main/java/com/nexters/goalpanzi/exception/UnauthorizedException.java
+++ src/main/java/com/nexters/goalpanzi/exception/UnauthorizedException.java
@@ -1,6 +1,11 @@
 package com.nexters.goalpanzi.exception;
 
 public class UnauthorizedException extends BaseException {
+
+    public UnauthorizedException(final ErrorCode errorCode) {
+        super(errorCode.getMessage());
+    }
+
     public UnauthorizedException(final String message) {
         super(message);
     }
