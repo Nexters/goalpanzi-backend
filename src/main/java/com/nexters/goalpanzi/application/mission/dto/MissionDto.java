57f4ea5414ad12c8b6ac914c5e795d2fe02ddd09
Songyi Kim <ksl2950@naver.com>
2024-07-28 02:05:50 +0900

    feat:wip mission dto 분리

---
+++ src/main/java/com/nexters/goalpanzi/application/mission/dto/MissionDto.java
@@ -0,0 +1,47 @@
+package com.nexters.goalpanzi.application.mission.dto;
+
+import com.nexters.goalpanzi.domain.mission.DayOfWeek;
+import com.nexters.goalpanzi.domain.mission.Mission;
+import com.nexters.goalpanzi.domain.mission.TimeOfDay;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class MissionDto {
+
+    public record Create(
+            Long hostMemberId,
+            String description,
+            LocalDateTime missionStartDate,
+            LocalDateTime missionEndDate,
+            TimeOfDay timeOfDay,
+            List<DayOfWeek> missionDays,
+            Integer boardCount
+    ) {
+    }
+
+    public record MissionResponse(
+            Long hostMemberId,
+            String description,
+            LocalDateTime missionStartDate,
+            LocalDateTime missionEndDate,
+            TimeOfDay timeOfDay,
+            List<DayOfWeek> missionDays,
+            Integer boardCount,
+            String invitationCode
+    ) {
+
+        public static MissionResponse from(final Mission mission) {
+            return new MissionResponse(
+                    mission.getHostMemberId(),
+                    mission.getDescription(),
+                    mission.getMissionStartDate(),
+                    mission.getMissionEndDate(),
+                    TimeOfDay.of(mission.getUploadStartTime(), mission.getUploadEndTime()),
+                    mission.getMissionDays(),
+                    mission.getBoardCount(),
+                    mission.getInvitationCode().getCode()
+            );
+        }
+    }
+}
