b30aa5bb569f3834235ecf235c8ce2e74001ff5d
kimyu0218 <serena35@ewhain.net>
2024-07-27 20:33:14 +0900

    feat: 생성자 추가

--- src/main/java/com/nexters/goalpanzi/exception/BadRequestException.java
+++ src/main/java/com/nexters/goalpanzi/exception/BadRequestException.java
@@ -1,6 +1,11 @@
 package com.nexters.goalpanzi.exception;
 
 public class BadRequestException extends BaseException {
+
+    public BadRequestException(final ErrorCode errorCode) {
+        super(errorCode.getMessage());
+    }
+    
     public BadRequestException(final String message) {
         super(message);
     }
