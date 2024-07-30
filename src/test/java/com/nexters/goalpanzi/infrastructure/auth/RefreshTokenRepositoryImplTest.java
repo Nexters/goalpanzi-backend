17966bed635b1b6a00bf863c2dfc2a4f528089b1
kimyu0218 <serena35@ewhain.net>
2024-07-30 20:08:33 +0900

    move: domain 하위 repository 디렉토리로 이동

--- src/test/java/com/nexters/goalpanzi/infrastructure/auth/RefreshTokenRepositoryImplTest.java
+++ src/test/java/com/nexters/goalpanzi/infrastructure/auth/RefreshTokenRepositoryImplTest.java
@@ -1,6 +1,6 @@
 //package com.nexters.goalpanzi.infrastructure.auth;
 //
-//import com.nexters.goalpanzi.domain.auth.RefreshTokenRepository;
+//import com.nexters.goalpanzi.domain.auth.repository.RefreshTokenRepository;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
