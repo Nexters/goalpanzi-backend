a0e8228dc190a2d807eb97297b68835cec35a069
Songyi Kim <ksl2950@naver.com>
2024-08-02 01:44:33 +0900

    chore: 스웨거 옵션 추가

--- src/main/java/com/nexters/goalpanzi/application/member/dto/response/ProfileResponse.java
+++ src/main/java/com/nexters/goalpanzi/application/member/dto/response/ProfileResponse.java
@@ -4,9 +4,9 @@ import com.nexters.goalpanzi.domain.member.CharacterType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ProfileResponse(
-        @Schema(description = "닉네임")
+        @Schema(description = "닉네임", requiredMode = Schema.RequiredMode.REQUIRED)
         String nickname,
-        @Schema(description = "캐릭터 타입")
+        @Schema(description = "캐릭터 타입", requiredMode = Schema.RequiredMode.REQUIRED)
         CharacterType characterType
 ) {
 }
--- src/main/java/com/nexters/goalpanzi/application/mission/dto/response/MissionResponse.java
+++ src/main/java/com/nexters/goalpanzi/application/mission/dto/response/MissionResponse.java
@@ -3,9 +3,9 @@ package com.nexters.goalpanzi.application.mission.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MissionResponse(
-        @Schema(description = "미션 ID")
+        @Schema(description = "미션 ID", requiredMode = Schema.RequiredMode.REQUIRED)
         Long missionId,
-        @Schema(description = "목표 행동")
+        @Schema(description = "목표 행동", requiredMode = Schema.RequiredMode.REQUIRED)
         String description
 ) {
 }
