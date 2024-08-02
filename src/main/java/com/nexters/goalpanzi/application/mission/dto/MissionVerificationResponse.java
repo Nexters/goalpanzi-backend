bf2b16a66b9d0986650dbece77c28ebd13257348
kimyu0218 <serena35@ewhain.net>
2024-08-02 19:31:43 +0900

    refactor: 인증 여부를 나타내는 flag 추가

--- src/main/java/com/nexters/goalpanzi/application/mission/dto/MissionVerificationResponse.java
+++ src/main/java/com/nexters/goalpanzi/application/mission/dto/MissionVerificationResponse.java
@@ -1,15 +1,24 @@
 package com.nexters.goalpanzi.application.mission.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.time.LocalDateTime;
 
 public record MissionVerificationResponse(
+        @Schema(description = "미션 인증 여부", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotNull Boolean isVerified,
+
         @Schema(description = "닉네임", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotEmpty String nickname,
 
         // TODO 캐릭터 이미지
 
-        @Schema(description = "인증 이미지 URL", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotEmpty String imageUrl
+        @Schema(description = "인증 이미지 URL", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+        String imageUrl,
+
+        @Schema(description = "인증 시간", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+        LocalDateTime verifiedAt
 ) {
 }
