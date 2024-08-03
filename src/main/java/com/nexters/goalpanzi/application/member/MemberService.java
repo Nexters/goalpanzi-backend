d2b63bf21dc7cee381813891ec28c0226854c8fc
Songyi Kim <ksl2950@naver.com>
2024-08-04 01:28:33 +0900

    chore: 불필요한 코드 제거

--- src/main/java/com/nexters/goalpanzi/application/member/MemberService.java
+++ src/main/java/com/nexters/goalpanzi/application/member/MemberService.java
@@ -6,7 +6,6 @@ import com.nexters.goalpanzi.domain.member.Member;
 import com.nexters.goalpanzi.domain.member.repository.MemberRepository;
 import com.nexters.goalpanzi.exception.AlreadyExistsException;
 import com.nexters.goalpanzi.exception.ErrorCode;
-import com.nexters.goalpanzi.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -22,14 +21,9 @@ public class MemberService {
     @Transactional
     public void updateProfile(final UpdateProfileCommand request) {
         validateNickname(request.nickname());
-        Member member = getMember(request.memberId());
+        Member member = memberRepository.getMember(request.memberId());
 
         member.updateProfile(request.nickname(), request.characterType());
-    }
-
-    private Member getMember(final Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MEMBER, memberId));
     }
 
     private void validateNickname(final String nickname) {
