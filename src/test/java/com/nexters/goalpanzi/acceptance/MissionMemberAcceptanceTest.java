712cad26bf9ff56dde79ad42951ce78060906788
kimyu0218 <serena35@ewhain.net>
2024-08-04 01:47:45 +0900

    remove: 불필요한 코드 제거

--- src/test/java/com/nexters/goalpanzi/acceptance/MissionMemberAcceptanceTest.java
+++ src/test/java/com/nexters/goalpanzi/acceptance/MissionMemberAcceptanceTest.java
@@ -4,11 +4,9 @@ import com.nexters.goalpanzi.application.auth.dto.request.GoogleLoginCommand;
 import com.nexters.goalpanzi.application.auth.dto.response.LoginResponse;
 import com.nexters.goalpanzi.application.mission.dto.response.MissionDetailResponse;
 import com.nexters.goalpanzi.application.mission.dto.response.MissionsResponse;
-import com.nexters.goalpanzi.infrastructure.ncp.ObjectStorageManager;
 import com.nexters.goalpanzi.presentation.mission.dto.JoinMissionRequest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,9 +17,6 @@ import static com.nexters.goalpanzi.fixture.TokenFixture.BEARER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MissionMemberAcceptanceTest extends AcceptanceTest {
-
-    @MockBean
-    ObjectStorageManager objectStorageManager;
 
     @Test
     void 초대코드로_미션에_참여한다() {
@@ -39,7 +34,7 @@ public class MissionMemberAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    void 참여하고있는_미션을_조회한다() {
+    void 참여하고_있는_미션을_조회한다() {
         LoginResponse login = 구글_로그인(new GoogleLoginCommand(EMAIL)).as(LoginResponse.class);
         MissionDetailResponse mission = 미션_생성(login.accessToken()).as(MissionDetailResponse.class);
         미션_참여(mission.invitationCode(), login.accessToken());
