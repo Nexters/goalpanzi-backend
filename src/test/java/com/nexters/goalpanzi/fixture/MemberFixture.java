5c2631bac2673e1eee46050afe09d52bb60defba
kimyu0218 <serena35@ewhain.net>
2024-08-04 14:44:33 +0900

    test: 미션 인증 테스트 작성

--- src/test/java/com/nexters/goalpanzi/fixture/MemberFixture.java
+++ src/test/java/com/nexters/goalpanzi/fixture/MemberFixture.java
@@ -3,6 +3,7 @@ package com.nexters.goalpanzi.fixture;
 public class MemberFixture {
     public static final Long MEMBER_ID = 1L;
     public static final String EMAIL = "test@gmail.com";
+    public static final String EMAIL2 = "test2@gmail.com";
     public static final String SOCIAL_ID = "12345";
     public static final String NICKNAME = "song2";
 }
