5c79b2acc36e6857d51564048223eb5e37d7be89
kimyu0218 <serena35@ewhain.net>
2024-08-04 14:43:41 +0900

    fix: java.time.DayOfWeek 호환 문제 해결

--- src/main/java/com/nexters/goalpanzi/domain/mission/DayOfWeek.java
+++ src/main/java/com/nexters/goalpanzi/domain/mission/DayOfWeek.java
@@ -7,6 +7,26 @@ public enum DayOfWeek {
     THURSDAY,
     FRIDAY,
     SATURDAY,
-    SUNDAY
-    ;
+    SUNDAY;
+
+    public static DayOfWeek fromJavaDayOfWeek(java.time.DayOfWeek dayOfWeek) {
+        switch (dayOfWeek) {
+            case MONDAY:
+                return MONDAY;
+            case TUESDAY:
+                return TUESDAY;
+            case WEDNESDAY:
+                return WEDNESDAY;
+            case THURSDAY:
+                return THURSDAY;
+            case FRIDAY:
+                return FRIDAY;
+            case SATURDAY:
+                return SATURDAY;
+            case SUNDAY:
+                return SUNDAY;
+            default:
+                throw new IllegalArgumentException("Invalid day of week: " + dayOfWeek);
+        }
+    }
 }
