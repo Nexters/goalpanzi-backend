3a274c478a09135e360ea5ab2594253eb201441d
Songyi Kim <ksl2950@naver.com>
2024-08-10 02:32:09 +0900

    feat: 동일한 인증 횟수인 경우 동일 순위로 지정

--- src/main/java/com/nexters/goalpanzi/application/mission/dto/response/MemberRankResponse.java
+++ src/main/java/com/nexters/goalpanzi/application/mission/dto/response/MemberRankResponse.java
@@ -4,20 +4,12 @@ import com.nexters.goalpanzi.domain.mission.MemberRank;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MemberRankResponse(
-        @Schema(description = "사용자 ID")
-        Long memberId,
-
-        @Schema(description = "사용자 닉네임")
-        String nickname,
-
         @Schema(description = "미션 최종 순위")
         Integer rank
 ) {
 
     public static MemberRankResponse from(final MemberRank memberRank) {
         return new MemberRankResponse(
-                memberRank.member().getId(),
-                memberRank.member().getNickname(),
                 memberRank.rank()
         );
     }
--- src/main/java/com/nexters/goalpanzi/domain/mission/MemberRanks.java
+++ src/main/java/com/nexters/goalpanzi/domain/mission/MemberRanks.java
@@ -14,25 +14,36 @@ public class MemberRanks {
     private final List<MemberRank> memberRanks;
 
     public static MemberRanks from(final List<MissionMember> missionMembers) {
-        List<MissionMember> orderedMembersByVerification = missionMembers
-                .stream()
-                .sorted((m1, m2) -> m2.getVerificationCount() - m1.getVerificationCount())
-                .toList();
+        List<MissionMember> sortedMissionMembers = sortedMembersByVerificationCountDesc(missionMembers);
 
         List<MemberRank> memberRanks = new ArrayList<>();
-        for (int i = 0; i < orderedMembersByVerification.size(); i++) {
-            MissionMember missionMember = orderedMembersByVerification.get(i);
-            memberRanks.add(new MemberRank(missionMember.getMember(), i + 1));
+        int rank = 1;
+        int previousVerificationCount = sortedMissionMembers.getFirst().getVerificationCount();
+
+        for (int i = 0; i < sortedMissionMembers.size(); i++) {
+            MissionMember missionMember = sortedMissionMembers.get(i);
+            if (missionMember.getVerificationCount() < previousVerificationCount) {
+                rank = i + 1;
+            }
+            memberRanks.add(new MemberRank(missionMember.getMember(), rank));
+            previousVerificationCount = missionMember.getVerificationCount();
         }
 
         return new MemberRanks(memberRanks);
+    }
+
+    private static List<MissionMember> sortedMembersByVerificationCountDesc(final List<MissionMember> missionMembers) {
+        return missionMembers
+                .stream()
+                .sorted((m1, m2) -> m2.getVerificationCount() - m1.getVerificationCount())
+                .toList();
     }
 
     public MemberRank getRankByMember(final Member member) {
         return memberRanks.stream()
                 .filter(memberRank -> memberRank.member().equals(member))
                 .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("No rank found for member " + member));
+                .orElseThrow(() -> new NoSuchElementException("No rank found for member " + member.getId()));
     }
 
     @Override
