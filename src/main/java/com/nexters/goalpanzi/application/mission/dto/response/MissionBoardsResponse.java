51f60d60048f5f3d3c92325d3e739dbf02c066bf
kimyu0218 <serena35@ewhain.net>
2024-08-12 01:48:51 +0900

    feat: rank 추가

--- src/main/java/com/nexters/goalpanzi/application/mission/dto/response/MissionBoardsResponse.java
+++ src/main/java/com/nexters/goalpanzi/application/mission/dto/response/MissionBoardsResponse.java
@@ -8,6 +8,8 @@ import java.util.List;
 public record MissionBoardsResponse(
         @Schema(description = "오늘 한 칸 전진한 멤버 수", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull Integer progressCount,
+        @Schema(description = "나의 꾸준함 순위", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotNull Integer rank,
         @Schema(description = "미션 보드칸 정보 리스트", requiredMode = Schema.RequiredMode.REQUIRED)
         List<MissionBoardResponse> missionBoards
 ) {
