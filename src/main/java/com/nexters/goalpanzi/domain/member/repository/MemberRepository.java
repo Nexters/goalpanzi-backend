93b290e6b50f99e12e94d336d624630518c8a549
Songyi Kim <ksl2950@naver.com>
2024-08-12 01:20:10 +0900

    chore: final 추가

--- src/main/java/com/nexters/goalpanzi/domain/member/repository/MemberRepository.java
+++ src/main/java/com/nexters/goalpanzi/domain/member/repository/MemberRepository.java
@@ -9,14 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findBySocialId(String socialId);
+    Optional<Member> findBySocialId(final String socialId);
 
-    Optional<Member> findByNickname(String nickname);
+    Optional<Member> findByNickname(final String nickname);
 
     @Query(value = "select m.* from member m where m.social_id = :socialId and m.deleted_at is not null", nativeQuery = true)
-    Optional<Member> findBySocialIdAndDeletedAtIsNotNull(String socialId);
+    Optional<Member> findBySocialIdAndDeletedAtIsNotNull(final String socialId);
 
-    default Member getMember(Long memberId) {
+    default Member getMember(final Long memberId) {
         return findById(memberId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MEMBER, memberId));
     }
