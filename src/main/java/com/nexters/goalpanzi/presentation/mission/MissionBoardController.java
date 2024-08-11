cfbe33102000e26935e52d58c8707360fa776a4b
kimyu0218 <serena35@ewhain.net>
2024-08-12 02:24:37 +0900

    remove: controller 쪽 schema 삭제

--- src/main/java/com/nexters/goalpanzi/presentation/mission/MissionBoardController.java
+++ src/main/java/com/nexters/goalpanzi/presentation/mission/MissionBoardController.java
@@ -4,7 +4,6 @@ import com.nexters.goalpanzi.application.mission.MissionBoardService;
 import com.nexters.goalpanzi.application.mission.dto.request.MissionBoardQuery;
 import com.nexters.goalpanzi.application.mission.dto.response.MissionBoardsResponse;
 import com.nexters.goalpanzi.common.argumentresolver.LoginMemberId;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +21,6 @@ public class MissionBoardController implements MissionBoardControllerDocs {
             @LoginMemberId final Long memberId,
             @PathVariable(name = "missionId") final Long missionId,
             @RequestParam(name = "sortType", required = false) final MissionBoardQuery.SortType sortType,
-            @Schema(description = "미션 보드칸 장기말 정렬 방향", requiredMode = Schema.RequiredMode.REQUIRED)
             @RequestParam(name = "sortDirection", required = false) final Sort.Direction direction
     ) {
         MissionBoardsResponse response = missionBoardService.getBoard(new MissionBoardQuery(memberId, missionId, sortType, direction));
