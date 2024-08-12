b6589b0645e086b3e65f5f494bc4a64be1b90dd7
kimyu0218 <serena35@ewhain.net>
2024-08-12 23:47:32 +0900

    comment: S3Client 테스트 모킹으로 변경

--- src/test/java/com/nexters/goalpanzi/infrastructure/aws/S3ClientTest.java
+++ src/test/java/com/nexters/goalpanzi/infrastructure/aws/S3ClientTest.java
@@ -1,61 +1,30 @@
-//package com.nexters.goalpanzi.infrastructure.aws;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest
-//class S3ClientTest {
-//
-//    @Autowired
-//    private S3Client s3Client;
-//
-//    private File tempFile;
-//    private MultipartFile imageFile;
-//    private String uploadedFileUrl;
-//
-//    @BeforeEach
-//    void 테스트를_위한_이미지_파일을_생성한다() throws IOException {
-//        tempFile = File.createTempFile("test-image", ".jpg");
-//
-//        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
-//            fos.write("이미지 파일 내용".getBytes());
-//        }
-//
-//        imageFile = new MockMultipartFile(
-//                "file",
-//                tempFile.getName(),
-//                "image/jpeg",
-//                new FileInputStream(tempFile)
-//        );
-//    }
-//
-//    @AfterEach
-//    public void 테스트_후_업로드된_이미지_파일을_삭제한다() {
-//        s3Client.deleteFile(uploadedFileUrl);
-//
-//        if (tempFile != null && tempFile.exists()) {
-//            tempFile.delete();
-//        }
-//    }
-//
-//    @Test
-//    public void 파일을_업로드한다() {
-//        uploadedFileUrl = s3Client.uploadFile(imageFile);
-//
-//        assertThat(uploadedFileUrl).isNotNull();
-//
-//        System.out.println(uploadedFileUrl);
-//    }
-//}
+package com.nexters.goalpanzi.infrastructure.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.multipart.MultipartFile;
+
+import static com.nexters.goalpanzi.fixture.MissionFixture.IMAGE_FILE;
+import static com.nexters.goalpanzi.fixture.MissionFixture.UPLOADED_IMAGE_URL;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class S3ClientTest {
+
+    @MockBean
+    private S3Client s3Client;
+
+    @Test
+    public void 파일을_업로드한다() {
+        when(s3Client.uploadFile(any(MultipartFile.class))).thenReturn(UPLOADED_IMAGE_URL);
+
+        String uploadedFileUrl = s3Client.uploadFile(IMAGE_FILE);
+
+        assertThat(uploadedFileUrl).isEqualTo(UPLOADED_IMAGE_URL);
+
+        System.out.println(uploadedFileUrl);
+    }
+}
