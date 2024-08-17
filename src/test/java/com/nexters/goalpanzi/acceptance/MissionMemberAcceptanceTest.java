1d46505c75e0fb618d58bc07bfe2a8de7bd7eefa
Songyi Kim <ksl2950@naver.com>
2024-08-18 04:49:57 +0900

    test: 테스트 코드 추가

--- src/test/java/com/nexters/goalpanzi/acceptance/MissionMemberAcceptanceTest.java
+++ src/test/java/com/nexters/goalpanzi/acceptance/MissionMemberAcceptanceTest.java
@@ -4,7 +4,11 @@ import com.nexters.goalpanzi.application.auth.dto.request.GoogleLoginCommand;
 import com.nexters.goalpanzi.application.auth.dto.response.LoginResponse;
 import com.nexters.goalpanzi.application.mission.dto.response.MissionDetailResponse;
 import com.nexters.goalpanzi.application.mission.dto.response.MissionsResponse;
+import com.nexters.goalpanzi.domain.mission.DayOfWeek;
+import com.nexters.goalpanzi.domain.mission.TimeOfDay;
 import com.nexters.goalpanzi.domain.mission.repository.MissionMemberRepository;
+import com.nexters.goalpanzi.exception.ErrorResponse;
+import com.nexters.goalpanzi.presentation.mission.dto.CreateMissionRequest;
 import com.nexters.goalpanzi.presentation.mission.dto.JoinMissionRequest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
@@ -13,10 +17,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static com.nexters.goalpanzi.acceptance.AcceptanceStep.구글_로그인;
 import static com.nexters.goalpanzi.acceptance.AcceptanceStep.미션_생성;
+import static com.nexters.goalpanzi.exception.ErrorCode.CAN_NOT_JOIN_MISSION;
 import static com.nexters.goalpanzi.fixture.MemberFixture.EMAIL_HOST;
 import static com.nexters.goalpanzi.fixture.MemberFixture.EMAIL_MEMBER_A;
+import static com.nexters.goalpanzi.fixture.MissionFixture.DESCRIPTION;
 import static com.nexters.goalpanzi.fixture.TokenFixture.BEARER;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -60,5 +69,21 @@ public class MissionMemberAcceptanceTest extends AcceptanceTest {
                 .as(MissionsResponse.class);
 
         assertThat(actual.missions()).hasSize(1);
+    }
+
+    @Test
+    void 미션기간이_아닌경우_참여가_불가능하다() {
+        LoginResponse loginHost = 구글_로그인(new GoogleLoginCommand(EMAIL_HOST)).as(LoginResponse.class);
+        CreateMissionRequest request = new CreateMissionRequest(DESCRIPTION, LocalDateTime.now(), LocalDateTime.now().plusDays(5), TimeOfDay.EVERYDAY, List.of(DayOfWeek.FRIDAY), 5);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, BEARER + loginHost.accessToken())
+                .body(request)
+                .when().post("/api/missions")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .onFailMessage(CAN_NOT_JOIN_MISSION.toString())
+                .extract();
     }
 }
