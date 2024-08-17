094ccd151659a1f52557b320c51d79bc74495a34
Songyi Kim <52441906+songyi00@users.noreply.github.com>
2024-08-17 20:51:53 +0900

    feat: redis testcontainer 추가 및 빌드 테스트 오류 해결 (#59)
    * test: 테스트 코드 정리
    
    * test:wip 실패하는 테스트
    
    * test:wip 실패하는 테스트
    
    * test: 성공하는 테스트
    
    * feat: redis test container 추가

--- src/test/java/com/nexters/goalpanzi/acceptance/AcceptanceTest.java
+++ src/test/java/com/nexters/goalpanzi/acceptance/AcceptanceTest.java
@@ -1,16 +1,21 @@
 package com.nexters.goalpanzi.acceptance;
 
+import com.nexters.goalpanzi.config.redis.RedisInitializer;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 
 @Sql("/truncate.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@ContextConfiguration(
+        initializers = {RedisInitializer.class}
+)
 public abstract class AcceptanceTest {
 
     @LocalServerPort
