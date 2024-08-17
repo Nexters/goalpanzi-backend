612aea63b5b10af524fd184ab47d5e591591f5f7
Songyi Kim <52441906+songyi00@users.noreply.github.com>
2024-08-17 18:07:06 +0900

    feat: DB 저장 날짜 포맷 고정 (#58)
    * feat: 날짜 포맷팅
    
    - ios 디코딩 이슈로 날짜 포맷 통일
    
    * chore: 임시 배포
    
    * chore: 임시 배포 코드 제거
    
    ---------
    
    Co-authored-by: Songyi Kim <songyi00@29cm.co.kr>

--- src/main/java/com/nexters/goalpanzi/domain/common/BaseEntity.java
+++ src/main/java/com/nexters/goalpanzi/domain/common/BaseEntity.java
@@ -1,19 +1,19 @@
 package com.nexters.goalpanzi.domain.common;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
@@ -25,6 +25,17 @@ public class BaseEntity {
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.parse(LocalDateTime.now().format(FORMATTER));
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.updatedAt = LocalDateTime.parse(LocalDateTime.now().format(FORMATTER));
+    }
 
     public void delete() {
         this.deletedAt = LocalDateTime.now();
