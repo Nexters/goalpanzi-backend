3af6bd38177a516245a4c43fcf1ccfc7e57c6369
Songyi Kim <ksl2950@naver.com>
2024-08-18 00:49:18 +0900

    fix: 빌드 실패 에러 해결

--- src/main/java/com/nexters/goalpanzi/domain/common/BaseEntity.java
+++ src/main/java/com/nexters/goalpanzi/domain/common/BaseEntity.java
@@ -1,10 +1,12 @@
 package com.nexters.goalpanzi.domain.common;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
