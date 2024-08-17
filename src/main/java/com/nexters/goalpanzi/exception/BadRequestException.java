7435e70c06b9f5e6a5a07c557acbb3cdd0a0bc52
kimyu0218 <serena35@ewhain.net>
2024-08-17 20:20:47 +0900

    refactor: 미션 참여 제한을 위한 에러 코드 추가

--- src/main/java/com/nexters/goalpanzi/exception/BadRequestException.java
+++ src/main/java/com/nexters/goalpanzi/exception/BadRequestException.java
@@ -5,7 +5,11 @@ public class BadRequestException extends BaseException {
     public BadRequestException(final ErrorCode errorCode) {
         super(errorCode.getMessage());
     }
-    
+
+    public BadRequestException(final ErrorCode errorCode, final Object... args) {
+        super(errorCode, args);
+    }
+
     public BadRequestException(final String message) {
         super(message);
     }
