6e348d0df861e37784b071289399633db4349c0b
kimyu0218 <serena35@ewhain.net>
2024-10-30 20:57:17 +0900

    feat: firebase 설정 정의

---
+++ src/main/java/com/nexters/goalpanzi/config/FirebaseConfig.java
@@ -0,0 +1,24 @@
+package com.nexters.goalpanzi.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-adminsdk.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+}
