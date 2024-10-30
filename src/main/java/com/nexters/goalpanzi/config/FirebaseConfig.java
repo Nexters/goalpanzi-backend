6e86364fd5a04de315f0a7abf9c503e439ce04bd
kimyu0218 <serena35@ewhain.net>
2024-10-30 21:36:39 +0900

    refactor: admin sdk 주입 방식 변경

--- src/main/java/com/nexters/goalpanzi/config/FirebaseConfig.java
+++ src/main/java/com/nexters/goalpanzi/config/FirebaseConfig.java
@@ -3,21 +3,27 @@ package com.nexters.goalpanzi.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.admin-sdk}")
+    private String encodedFirebaseAdminSdk;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-adminsdk.json");
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedFirebaseAdminSdk);
+        ByteArrayInputStream adminSdk = new ByteArrayInputStream(decodedBytes);
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(adminSdk))
                 .build();
         return FirebaseApp.initializeApp(options);
     }
