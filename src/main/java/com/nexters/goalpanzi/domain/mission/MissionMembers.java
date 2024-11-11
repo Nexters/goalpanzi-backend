3883f9a05782bc6e81fb035bd4af26d780cb4d0b
kimyu0218 <serena35@ewhain.net>
2024-11-11 20:23:31 +0900

    fix: 오늘 미션 인증한 사람 수 세는 로직 수정

--- src/main/java/com/nexters/goalpanzi/domain/mission/MissionMembers.java
+++ src/main/java/com/nexters/goalpanzi/domain/mission/MissionMembers.java
@@ -6,20 +6,13 @@ import com.nexters.goalpanzi.exception.ForbiddenException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Getter
 public class MissionMembers {
+    
     private final List<MissionMember> missionMembers;
-
-    public Integer getProgressCount() {
-        return missionMembers.stream()
-                .filter(missionMember ->
-                        missionMember.getUpdatedAt().toLocalDate().equals(LocalDate.now()))
-                .toList().size();
-    }
 
     public List<MissionMember> getMissionMembersByBoardNumber(final Integer boardNumber) {
         return missionMembers.stream()
