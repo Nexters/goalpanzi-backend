956969a7888a63b0a1a3e9f1e464e8600f11d5cd
kimyu0218 <serena35@ewhain.net>
2024-11-16 13:43:57 +0900

    feat: 분산락에 사용될 key 정의

---
+++ src/main/java/com/nexters/goalpanzi/infrastructure/redisson/LockKey.java
@@ -0,0 +1,23 @@
+package com.nexters.goalpanzi.infrastructure.redisson;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+public class LockKey {
+    
+    public static final String LOCK_PREFIX = "LOCK";
+
+    private final String target;
+    private final String value;
+
+    public static LockKey of(final String target, final Object... args) {
+        return new LockKey(target, Arrays.toString(args));
+    }
+
+    @Override
+    public String toString() {
+        return LOCK_PREFIX + ":" + target + ":" + value;
+    }
+}
