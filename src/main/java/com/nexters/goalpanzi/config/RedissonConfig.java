786a6a397a8c2f35e92a565eeb71c535875552d2
kimyu0218 <serena35@ewhain.net>
2024-11-16 14:53:21 +0900

    fix: 테스트 환경에서 RedissonConfig 로드하지 않도록 수정

--- src/main/java/com/nexters/goalpanzi/config/RedissonConfig.java
+++ src/main/java/com/nexters/goalpanzi/config/RedissonConfig.java
@@ -4,9 +4,11 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty(name = "spring.data.redis.redisson.enabled", havingValue = "true")
 @Configuration
 public class RedissonConfig {
 
