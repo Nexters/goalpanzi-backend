ad8e55c88301d00481bc5695b5e10749406a8994
kimyu0218 <serena35@ewhain.net>
2024-11-19 09:58:48 +0900

    remove: 불필요한 중괄호 삭제

--- src/main/java/com/nexters/goalpanzi/common/annotation/RedissonLock.java
+++ src/main/java/com/nexters/goalpanzi/common/annotation/RedissonLock.java
@@ -3,13 +3,13 @@ package com.nexters.goalpanzi.common.annotation;
 import java.lang.annotation.*;
 import java.util.concurrent.TimeUnit;
 
-@Target({ElementType.METHOD})
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface RedissonLock {
     String value() default "Unknown";
 
-    long waitTime() default 5;
+    long waitTime() default 5L;
 
     TimeUnit timeUnit() default TimeUnit.SECONDS;
 }
