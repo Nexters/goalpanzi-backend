65a5abebfea0d32187b67e981925d06cb037005b
kimyu0218 <serena35@ewhain.net>
2024-11-30 17:07:52 +0900

    test: 디바이스 토큰 및 활성화 상태 갱신 시 발생하는 이벤트 단위 테스트 작성

---
+++ src/test/java/com/nexters/goalpanzi/application/mission/event/handler/MissionMemberEventHandlerTest.java
@@ -0,0 +1,100 @@
+package com.nexters.goalpanzi.application.mission.event.handler;
+
+import com.nexters.goalpanzi.application.member.event.UpdateDeviceTokenEvent;
+import com.nexters.goalpanzi.application.member.event.UpdatePushActivationStatusEvent;
+import com.nexters.goalpanzi.application.mission.MissionMemberService;
+import com.nexters.goalpanzi.application.mission.MissionVerificationService;
+import com.nexters.goalpanzi.config.event.SyncEventConfig;
+import com.nexters.goalpanzi.config.redis.RedisInitializer;
+import com.nexters.goalpanzi.infrastructure.firebase.PushNotificationSender;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(
+        classes = {SyncEventConfig.class}
+)
+@ContextConfiguration(
+        initializers = {RedisInitializer.class}
+)
+class MissionMemberEventHandlerTest {
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @Autowired
+    private MissionMemberEventHandler missionMemberEventHandler;
+
+    @MockBean
+    private MissionMemberService missionMemberService;
+
+    @MockBean
+    private MissionVerificationService missionVerificationService;
+
+    @MockBean
+    private PushNotificationSender pushNotificationSender;
+
+    @Test
+    void 기존_디바이스_토큰이_null인_상황에서_디바이스_토큰을_갱신하면_토픽_구독만_진행한다() {
+        UpdateDeviceTokenEvent event = new UpdateDeviceTokenEvent(1L, null, "deviceToken");
+
+        transactionTemplate.execute(status -> {
+            applicationEventPublisher.publishEvent(event);
+            return null;
+        });
+
+        verify(missionMemberService, times(1))
+                .subscribeToMyMissions(event.memberId(), event.deviceToken());
+    }
+
+    @Test
+    void 기존_디바이스_토큰이_null이_아닌_상황에서_디바이스_토큰을_갱신하면_기존_디바이스_토큰이_구독한_토픽을_구독_취소하고_새로운_디바이스_토큰으로_토픽을_구독한다() {
+        UpdateDeviceTokenEvent event = new UpdateDeviceTokenEvent(1L, "deprecatedDeviceToken", "deviceToken");
+
+        transactionTemplate.execute(status -> {
+            applicationEventPublisher.publishEvent(event);
+            return null;
+        });
+
+        verify(missionMemberService, times(1))
+                .unsubscribeFromMyMissions(event.memberId(), event.deprecatedDeviceToken());
+        verify(missionMemberService, times(1))
+                .subscribeToMyMissions(event.memberId(), event.deviceToken());
+    }
+
+    @Test
+    void 푸시_알림을_활성화하는_경우_토픽을_구독한다() {
+        UpdatePushActivationStatusEvent event = new UpdatePushActivationStatusEvent(1L, "deviceToken", true);
+
+        transactionTemplate.execute(status -> {
+            applicationEventPublisher.publishEvent(event);
+            return null;
+        });
+
+        verify(missionMemberService, times(1))
+                .subscribeToMyMissions(event.memberId(), event.deviceToken());
+    }
+
+    @Test
+    void 푸시_알림을_비활성화하는_경우_토픽을_구독_취소한다() {
+        UpdatePushActivationStatusEvent event = new UpdatePushActivationStatusEvent(1L, "deviceToken", false);
+
+        transactionTemplate.execute(status -> {
+            applicationEventPublisher.publishEvent(event);
+            return null;
+        });
+
+        verify(missionMemberService, times(1))
+                .unsubscribeFromMyMissions(event.memberId(), event.deviceToken());
+    }
+}
---
+++ src/test/java/com/nexters/goalpanzi/config/event/SyncEventConfig.java
@@ -0,0 +1,16 @@
+package com.nexters.goalpanzi.config.event;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+
+import java.util.concurrent.Executor;
+
+@TestConfiguration
+public class SyncEventConfig extends AsyncConfigurerSupport {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return new SyncTaskExecutor();
+    }
+}
