43ecbb888c537bddb96e35b65603b986fc06d537
kimyu0218 <serena35@ewhain.net>
2024-12-13 12:11:04 +0900

    refactor: 함수 분리

--- src/main/java/com/nexters/goalpanzi/application/mission/MissionMemberService.java
+++ src/main/java/com/nexters/goalpanzi/application/mission/MissionMemberService.java
@@ -62,15 +62,8 @@ public class MissionMemberService {
         missionValidator.validateMaxPersonnel(mission);
         missionMemberRepository.save(MissionMember.join(member, mission));
 
-        Long hostMemberId = mission.getHostMemberId();
-        Member hostMember = memberRepository.getMember(hostMemberId);
-        if (hostMember.isPushActivated() && !mission.isHostMember(memberId)) {
-            eventPublisher.publishEvent(
-                    new JoinMissionEvent(mission.getId(), hostMember.getDeviceToken(), member.getNickname())
-            );
-        }
+        sendJoinPushMessage(member, mission);
 
-        // TODO: 추후 필요없으면 삭제
         if (!member.isPushActivated()) {
             cancelRetryPushMessage(member.getId());
         }
@@ -86,6 +79,16 @@ public class MissionMemberService {
                 .ifPresent(missionMember -> {
                     throw new AlreadyExistsException(ErrorCode.ALREADY_EXISTS_MISSION_MEMBER);
                 });
+    }
+
+    private void sendJoinPushMessage(final Member member, final Mission mission) {
+        Member hostMember = memberRepository.getMember(mission.getHostMemberId());
+
+        if (hostMember.isPushActivated() && !mission.isHostMember(member.getId())) {
+            eventPublisher.publishEvent(
+                    new JoinMissionEvent(mission.getId(), hostMember.getDeviceToken(), member.getNickname())
+            );
+        }
     }
 
     public MissionsResponse findAllByMemberId(final Long memberId, final List<MissionStatus> filter) {
