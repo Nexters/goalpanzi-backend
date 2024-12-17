66abd5266e7c626d6524c4848e5417aa58d48875
kimyu0218 <serena35@naver.com>
2024-12-17 16:32:00 +0900

    feat: 디바이스 일급 컬렉션 작성

---
+++ src/main/java/com/nexters/goalpanzi/domain/firebase/Devices.java
@@ -0,0 +1,30 @@
+package com.nexters.goalpanzi.domain.firebase;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class Devices {
+
+    private final List<Device> devices;
+
+    public List<Device> getActivatedDevices() {
+        return devices.stream()
+                .filter(Device::getPushActivationStatus)
+                .toList();
+    }
+
+    public List<String> getActivatedDeviceTokens() {
+        return getActivatedDevices().stream()
+                .map(Device::getDeviceToken)
+                .toList();
+    }
+
+    public List<String> getDeactivatedDeviceTokens() {
+        return devices.stream()
+                .filter(it -> !it.getPushActivationStatus())
+                .map(Device::getDeviceToken)
+                .toList();
+    }
+}
