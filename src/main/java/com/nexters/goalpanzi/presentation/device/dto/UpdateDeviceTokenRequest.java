b941ce697cc1998f143b34c58bb7633490751c98
kimyu0218 <serena35@naver.com>
2024-12-19 13:47:49 +0900

    chore: 유효성 검사 추가

--- src/main/java/com/nexters/goalpanzi/presentation/device/dto/UpdateDeviceTokenRequest.java
+++ src/main/java/com/nexters/goalpanzi/presentation/device/dto/UpdateDeviceTokenRequest.java
@@ -3,14 +3,15 @@ package com.nexters.goalpanzi.presentation.device.dto;
 import com.nexters.goalpanzi.application.device.dto.request.UpdateDeviceTokenCommand;
 import com.nexters.goalpanzi.domain.device.OsType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 
 public record UpdateDeviceTokenRequest(
         @Schema(description = "디바이스 식별자", requiredMode = Schema.RequiredMode.REQUIRED)
-        String deviceIdentifier,
+        @NotEmpty String deviceIdentifier,
         @Schema(description = "device token", requiredMode = Schema.RequiredMode.REQUIRED)
-        String deviceToken,
+        @NotEmpty String deviceToken,
         @Schema(description = "디바이스 운영체제", requiredMode = Schema.RequiredMode.REQUIRED)
-        OsType osType
+        @NotEmpty OsType osType
 ) {
 
     public UpdateDeviceTokenCommand toServiceDto(final Long memberId) {
--- src/main/java/com/nexters/goalpanzi/presentation/device/dto/UpdatePushActivationStatusRequest.java
+++ src/main/java/com/nexters/goalpanzi/presentation/device/dto/UpdatePushActivationStatusRequest.java
@@ -2,11 +2,12 @@ package com.nexters.goalpanzi.presentation.device.dto;
 
 import com.nexters.goalpanzi.application.device.dto.request.UpdatePushActivationStatusCommand;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdatePushActivationStatusRequest(
         @Schema(description = "디바이스 식별자", requiredMode = Schema.RequiredMode.REQUIRED)
-        String deviceIdentifier,
+        @NotEmpty String deviceIdentifier,
         @Schema(description = "푸시 알림 활성화 여부", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull Boolean pushActivationStatus
 ) {
