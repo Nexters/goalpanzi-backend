ab6aadcc2a19e2709b023e38e37861e17c0c4219
kimyu0218 <serena35@naver.com>
2025-03-01 21:07:36 +0900

    remove: 불필요한 어노테이션 제거

--- src/main/java/com/nexters/goalpanzi/application/mission/dto/request/MissionVerificationQuery.java
+++ src/main/java/com/nexters/goalpanzi/application/mission/dto/request/MissionVerificationQuery.java
@@ -1,14 +1,13 @@
 package com.nexters.goalpanzi.application.mission.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 
 public record MissionVerificationQuery(
-        @NotNull Long memberId,
-        @NotNull Long missionId,
+        Long memberId,
+        Long missionId,
         LocalDate date,
         SortType sortType,
         Sort.Direction direction
